a7ce30afd7575dfb86c63781cbb829ab9889261f
Chaion Saha <chaionsaha00@gmail.com>
2023-03-25 02:40:12 +0600

    login bug fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 require('dotenv').config();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 // MongoDB configs
 
@@ -28,14 +28,17 @@ async function run() {
 
 		////////////////////////////////////////////////
 		///// Login key
-		app.get('/login', async (req, res) => {
+		app.post('/login', async (req, res) => {
 			const body = req.body;
-			const token = jwt.sign(body, process.env.SECRET_KEY, {
-				expiresIn: '1d',
-			});
-
-			console.log(token);
-			res.send(token);
+			console.log(body);
+			if (!body) return res.status(400).send('Bad request');
+			else {
+				const token = jwt.sign(body, process.env.SECRET_KEY, {
+					expiresIn: '1d',
+				});
+				console.log(token);
+				res.send(token);
+			}
 		});
 		/////////////////////////////////////////////////
 		///// Products
